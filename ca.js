e7db538d84cfc11bef80321122b5f5a31dd0f4b4
Ben Patterson <bmizepatterson@gmail.com>
2018-08-28 15:42:28 -0400

    Add speed setting to change the frame rate

--- ca.js
+++ ca.js
@@ -121,6 +121,10 @@ function updateRuleset() {
 	document.getElementById('rs').innerHTML = newRule;
 }
 
+function updateFrameRate() {
+	frameRate(parseInt(document.getElementById('frameRate').value));
+}
+
 function initDoc() {
 	// Add event listeners
 	document.getElementById('step').addEventListener('click', step);
@@ -128,6 +132,7 @@ function initDoc() {
 	document.getElementById('pause').addEventListener('click', pause);
 	document.getElementById('resume').addEventListener('click', resume);
 	document.getElementById('saveCA').addEventListener('click', saveCA);
+	document.getElementById('frameRate').addEventListener('change', updateFrameRate);
 	// Populate ruleset select
 	let ruleset = document.getElementById('ruleset');
 	for (let i = 0; i < 256; i++) {
